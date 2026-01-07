85160fe6493129355fd10f0d1fb2d2b6d0d38fdb
rashomon <rashomon@noreply.codeberg.org>
2026-01-07 13:08:29 +0100

    secrets store

---
+++ alphaevolve/secrets.py
@@ -0,0 +1,12 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class SecretsStore(BaseSettings):
+    model_config = SettingsConfigDict(env_file="../.env", extra="ignore")
+
+    huggingface_token: SecretStr
+
+
+# export
+values = SecretsStore()  # type: ignore
